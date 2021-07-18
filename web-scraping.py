ca141ad00bd55177043ae1643145c74d399bc3e5
Rachana Murali Narayanan <rachanamn@Each2ooo.local>
2021-07-18 17:08:33 -0400

    added web scraping tool

---
+++ web-scraping.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+
+page = requests.get('https://web.archive.org/web/20121007172955/https://www.nga.gov/collection/anZ1.htm')
+soup = BeautifulSoup(page.text, 'html.parser')
+
+last_links = soup.find(class_='AlphaNav')
+last_links.decompose()
+
+artist_name_list = soup.find(class_='BodyText')
+artist_name_list_items = artist_name_list.find_all('a')
+
+
+# Pull text from all instances of <a> tag within BodyText div
+
+for artist_name in artist_name_list_items:
+    names = artist_name.contents[0]
+    print(names)
