78badfecdd1da541c19b93511c6acc1ed403608f
vasudhasrinivasan <vasudha.srini16@gmail.com>
2021-07-20 20:36:42 -0400

    tested web-scrapping, need to change url, prints all lines with new line char

--- web-scraping.py
+++ web-scraping.py
@@ -1,18 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
+import nltk
+import re
 
-page = requests.get('https://web.archive.org/web/20121007172955/https://www.nga.gov/collection/anZ1.htm')
-soup = BeautifulSoup(page.text, 'html.parser')
+URL = "https://www.noodles.com/covidupdate/"
+r = requests.get(URL)
+soup = BeautifulSoup(r.content, 'html5lib')
 
-last_links = soup.find(class_='AlphaNav')
-last_links.decompose()
+def find_by_label():
+    find = soup.findAll(text="COVID")
+    return find
 
-artist_name_list = soup.find(class_='BodyText')
-artist_name_list_items = artist_name_list.find_all('a')
+sentences = nltk.sent_tokenize(r.text)
+result = [sentence for sentence in sentences if "COVID" in sentence]
+print(result)
+find_by_label()
 
-
-# Pull text from all instances of <a> tag within BodyText div
-
-for artist_name in artist_name_list_items:
-    names = artist_name.contents[0]
-    print(names)
+if 'covid' in r.text:
+    print("This is available")
+else:
+    print("Not available")
